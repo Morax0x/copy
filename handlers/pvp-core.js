f9c979af232e285e558eadeaf1b13fe0fef38743
Morax0x <aziz6kd@gmail.com>
2025-12-22 13:38:13 +0400

    Update pvp-core.js

--- handlers/pvp-core.js
+++ handlers/pvp-core.js
@@ -5,6 +5,25 @@ const rootDir = process.cwd();
 const weaponsConfig = require(path.join(rootDir, 'json', 'weapons-config.json'));
 const skillsConfig = require(path.join(rootDir, 'json', 'skills-config.json'));
 
+// --- تعريف مصفوفات الصور ---
+const WIN_IMAGES = [
+    'https://i.postimg.cc/JhMrnyLd/download-1.gif',
+    'https://i.postimg.cc/FHgv29L0/download.gif',
+    'https://i.postimg.cc/9MzjRZNy/haru-midoriya.gif',
+    'https://i.postimg.cc/4ygk8q3G/tumblr-nmao11Zm-Bx1r3rdh2o2-500-gif-500-281.gif',
+    'https://i.postimg.cc/pL6NNpdC/Epic7-Epic-Seven-GIF-Epic7-Epic-Seven-Tensura-Discover-Share-GIFs.gif',
+    'https://i.postimg.cc/05dLktNF/download-5.gif',
+    'https://i.postimg.cc/sXRVMwhZ/download-2.gif'
+];
+
+const LOSE_IMAGES = [
+    'https://i.postimg.cc/xd8msjxk/escapar-a-toda-velocidad.gif',
+    'https://i.postimg.cc/1zb8JGVC/download.gif',
+    'https://i.postimg.cc/rmSwjvkV/download-1.gif',
+    'https://i.postimg.cc/8PyPZRqt/download.jpg'
+];
+// ---------------------------
+
 const EMOJI_MORA = '<:mora:1435647151349698621>';
 const BASE_HP = 100;
 const HP_PER_LEVEL = 4;
