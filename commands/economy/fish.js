d13656374c4c03eea00d146be54754717d708dfb
Morax0x <aziz6kd@gmail.com>
2026-01-15 17:32:56 +0400

    Update fish.js

--- commands/economy/fish.js
+++ commands/economy/fish.js
@@ -47,7 +47,7 @@ const FISHING_GIFS = [
     "https://i.postimg.cc/NMbn2v26/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f.gif"
 ];
 
-// 🔥 Set لتخزين اللاعبين النشطين (في الذاكرة)
+// 🔥 Set لتخزين اللاعبين النشطين (في الذكرة)
 const activeFishingSessions = new Set();
 
 module.exports = {
